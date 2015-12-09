e490f015c4ace908b3a9c777d048fecce42be881
PetersonRoboticSolutions <paulwpeterson12@gmail.com>
2015-12-08 19:06:02 -0500

    no message

--- GetThere/src/EndUserGUI.java
+++ GetThere/src/EndUserGUI.java
@@ -540,22 +540,22 @@ public class EndUserGUI extends JPanel implements ActionListener{
 				        doc.addAuthor("GetThere");
 				        doc.addTitle("This is title");
 				        doc.open();
-				         
+				        
 				        //adding a local image and aligned RIGHT
 				        for(int i = 0; i < totalMaps; i++) {
-				        	new PanelCapture(mapPanel);
-					        Image image = Image.getInstance("Screen.png");
+				        	new PanelCapture(mapPanel, i);
+					        Image image = Image.getInstance("Screen"+i+".png");
 				            doc.setPageSize(image);
 				            doc.newPage();
 				            image.setAbsolutePosition(0, 0);
+				            System.out.println(arrowCounter);
 					        doc.add(image);
-					        if(i!=totalMaps-1){
+					        if(i!=totalMaps){
 					        	rightArrow.getModel().setArmed(true);
 					        	rightArrow.getModel().setPressed(true);
 					        	rightArrow.getModel().setPressed(false);
-					        	
-					        }
-					        rightArrow.getModel().setArmed(false);
+					        	rightArrow.getModel().setArmed(false);
+					        }    
 				        }
 				
 				        doc.close();
